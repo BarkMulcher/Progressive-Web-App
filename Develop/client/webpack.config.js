03224b53954f46cc0ca4522c23b8074f9a023ed1
BarkMulcher <lucas.c.haskell@gmail.com>
2022-11-15 19:59:47 -0500

    add SW and manifest

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -18,12 +18,64 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'TODOs List'
+      }),
+      new WebpackPwaManifest({
+        name: 'My Progressive Web App',
+        short_name: 'MyPWA',
+        description: 'My awesome Progressive Web App!',
+        background_color: '#ffffff',
+        crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+        icons: [
+          {
+            src: path.resolve('src/assets/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+          }
+        ]
+      }),
+
+      new InjectManifest(),
+      new WebpackPwaManifest({
+        
+          "short_name": "Manifest",
+          "name": "TODOs Manifest Example",
+          "icons": [
+            {
+              "src": "./assets/images/logo.png",
+              "type": "image/png",
+              "sizes": "96x96",
+              "purpose": "any maskable"
+            }
+          ],
+          "orientation": "portrait",
+          "display": "standalone",
+          "start_url": "./",
+          "description": "Keep track of important tasks!",
+          "background_color": "#7eb4e2",
+          "theme_color": "#7eb4e2"
+        
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
